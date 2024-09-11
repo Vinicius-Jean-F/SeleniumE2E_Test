28c617345ab746f877bb892609a9e4a5ae20cf50
Vinicius <viniciusjeanferreira@gmail.com>
2024-09-11 17:47:54 -0300

    Add support to other navigators

--- conftest.py
+++ conftest.py
@@ -2,12 +2,24 @@ import pytest
 from selenium import webdriver 
 from selenium.webdriver.chrome.service import Service
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+
 @pytest.fixture(scope="class")
 def setup(request):
-    # Initialize the Chrome WebDriver
-    service_obj = Service("/usr/bin/chromedriver") 
-    driver = webdriver.Chrome(service=service_obj)
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        # Initialize the Chrome WebDriver
+        service_obj = Service("/usr/bin/chromedriver") 
+        driver = webdriver.Chrome(service=service_obj)
 
+    elif browser_name == "firefox":
+        i = 0
+    elif browser_name == "IE":
+        i = 0
+        
     # Navigate to the desired webpage
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     driver.maximize_window()
