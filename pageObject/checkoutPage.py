e3d550c7932e8a8394b7745abc53939328d07892
Vinicius <viniciusjeanferreira@gmail.com>
2024-09-11 23:25:59 -0300

    Using classes for each page

---
+++ pageObject/checkoutPage.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+class CheckoutPage:
+
+    cardtitles = (By.XPATH, "//app-card/div[@class='card h-100']/div[@class='card-body']/h4/a")
+    cardfooter = (By.XPATH, "div/button")
+    checkoutbutton = (By.XPATH, "//a[@class='nav-link btn btn-primary']")
+    successbutton = (By.XPATH, "//button[@class='btn btn-success']")
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def getCardTitles(self):
+        return self.driver.find_elements(*CheckoutPage.cardtitles)
+    
+    def clickCardFooter(self):
+        return self.driver.find_element(*CheckoutPage.cardfooter).click()
+        
+    def clickCheckoutButton(self):
+        return self.driver.find_element(*CheckoutPage.checkoutbutton).click()
+    
+    def clickSuccessButton(self):
+        return self.driver.find_element(*CheckoutPage.successbutton).click()
+
+
+    
---
+++ pageObject/homePage.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class HomePage:
+
+    shop = (By.CSS_SELECTOR, "a[href*='shop']")
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def shopItems(self, timeout=10):
+        # Wait until the element is present and clickable
+        element = WebDriverWait(self.driver, timeout).until(
+            EC.element_to_be_clickable(HomePage.shop)
+        )
+        return element
